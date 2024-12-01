3c4def5a16caa27bb6d88e8941899599b5f627ee
Tudor <tudor.petru.stefanescu@gmail.com>
2024-12-01 18:19:11 +0100

    day 1

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tudorescu/advent2024
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tudorescu/advent2024_test.clj
@@ -0,0 +1,7 @@
+(ns tudorescu.advent2024-test
+  (:require [clojure.test :refer :all]
+            [tudorescu.advent2024 :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
