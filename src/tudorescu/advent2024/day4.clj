777fe6ec743905b3bbf8d27d9103c3f107cd1fbb
Tudor <tudor.petru.stefanescu@gmail.com>
2024-12-10 06:24:56 +0100

    day4 input study

--- src/tudorescu/advent2024/day4.clj
+++ src/tudorescu/advent2024/day4.clj
@@ -42,21 +42,56 @@
    here's the same word search again, but where letters not involved 
    in any XMAS have been replaced with .:
    
-   ....XXMAS.
-   .SAMXMS...
-   ...S..A...
-   ..A.A.MS.X
-   XMASAMX.MM
-   X.....XA.A
-   S.S.S.S.SS
-   .A.A.A.A.A
-   ..M.M.M.MM
-   .X.X.XMASX
+    0123456789
+   0....XXMAS.
+   1.SAMXMS...
+   2...S..A...
+   3..A.A.MS.X
+   4XMASAMX.MM
+   5X.....XA.A
+   6S.S.S.S.SS
+   7.A.A.A.A.A
+   8..M.M.M.MM
+   9.X.X.XMASX
+
+  -3  -3  *  * 0 *  *  +3 
+  -2   * -2  * 0 *  +2  *
+  -1   *  * -1 0 +1  *  *
+   0  -3 -2 -1 X +1 +2 +3
+  +1   *  * -1 0 +1  *  *
+  +2   * -2  * 0  * +2  *
+  +3  -3  *  * 0  *  * +3
+   
+   1  (0 5, 0 6, 0 7, 0 8)
+   2  (0 4, 1 5, 2 6, 3 7)
+   3  (1 1, 1 2, 1 3, 1 4)
+   4  (1 6, 2 6, 3 6, 4 6)
+   5  (3 9, 4 9, 5 9, 6 9)
+   6  (3 9, 4 8, 5 7, 6 6)
+   7  (4 0, 4 1, 4 2, 4 3)
+   8  (4 3, 4 4, 4 5, 4 6) (SAMX)
+   9  (2 3, 3 2, 4 1, 5 0) (SAMX)
+   10 (5 6, 4 5, 3 4, 2 3)
+   11 (6 0, 7 1, 8 2, 9 3)
+   12 (6 2, 7 3, 8 4, 9 5)
+   13 (6 4, 7 3, 8 2, 9 1) (SAMX)
+   14 (6 6, 7 5, 8 4, 9 3) (SAMX)
+   15 (6 8, 7 7, 8 6, 9 5) (SAMX)
+   16 (9 5, 9 6, 9 7, 9 8)
+   17 (9 9, 8 9, 7 9, 6 9)
+   18 (9 9, 8 8, 7 7, 6 6)
+
+   
    Take a look at the little Elf's word search. How many times does XMAS appear?
    
    Idea: just count the number of X, M, A, S in the input string 
    and return the minimum of the counts raised to 4 (since XMAS has 4 letters)
  "
-  ([] (do-puzzle1 "resources/04_input.txt"))
+  ([] (do-puzzle1 (slurp "resources/04_input.txt")))
   ([input]
-   ))
+   (let [xmas-count (fn [input] (->> input (filter #{\X \M \A \S}) (frequencies)))]
+     (->> input
+          (xmas-count)
+          (vals)
+          (map #(Math/pow % 4))
+          (apply min)))))
